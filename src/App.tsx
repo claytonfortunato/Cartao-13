c6aba4016785f099186251d04fcf323fdb8d0a4f
claytonfortunato <claytonreis10@gmail.com>
2023-04-20 11:48:31 -0300

    feat: favicon and themes

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import { ThemeProvider } from "styled-components";
 import { GlobalStyle } from "./styles/global";
 import { defaultTheme } from "./styles/themes/default";
+import { Home } from "./pages/Home";
 
 function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
       <GlobalStyle />
+      <Home />
     </ThemeProvider>
   );
 }
--- src/styles/themes/default.ts
+++ src/styles/themes/default.ts
@@ -1,4 +1,13 @@
 export const defaultTheme = {
   white: "#fff",
   black: "#333",
+  gray200: "#E5E7EB",
+  gray300: "#D1D5DB",
+  gray400: "#9CA3AF",
+  gray700: "#374151",
+  gray800: "#1F2937",
+  gray900: "#111827",
+  purple: "#A855F7",
+  sucess: "#6EE7B7",
+  error: "#FB7185",
 };
